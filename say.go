8f657e1331f8819625b8d7482bf3e3c922b497fb
hanyxp <hanyxp@163.com>
2021-02-19 15:24:50 +0800

    tagB 2

--- say.go
+++ say.go
@@ -2,6 +2,8 @@ package god
 
 import "fmt"
 
-func Say(name string) string {
-	return fmt.Sprintf("vim-go %s", name)
+func Say(name string) {
+	str := fmt.Sprintf("vim-go %s", name)
+
+	fmt.Printf("out print: %s \n", str)
 }
